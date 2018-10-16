c667e9f1f9922520a284b403995aca8f84b597be
Shen Lin <shen.lin@outlook.com>
2018-10-15 18:01:53 -0700

    join room in progress

--- foosball-gaming/src/rooms/standard/StandardRoom.ts
+++ foosball-gaming/src/rooms/standard/StandardRoom.ts
@@ -4,7 +4,7 @@ export class StandardRoom extends Room<any> {
     standard: String
 
     onInit (options) {
-        var oMetadata = {name: options.standard, creator: options.creator};
+        var oMetadata = {name: options.standard, creator: options.creator, scene: options.scene, mode: options.mode};
         console.log("StandardRoom created!", options);
         this.setState({metadata: oMetadata});
         this.setMetadata(oMetadata);
@@ -16,7 +16,7 @@ export class StandardRoom extends Room<any> {
       }
 
     onJoin (client) {
-        console.log(client.id, client.sessionId, "joined successfully");
+        console.log("Client Id: ", client.id, "Client session: ",  client.sessionId, "joined successfully");
     }
 
     onLeave (client) {
@@ -24,11 +24,11 @@ export class StandardRoom extends Room<any> {
     }
 
     onMessage (client, data) {
-        console.log("AuthRoom received message from", client.sessionId, ":", data);
+        console.log("Room received message from client session", client.sessionId, ":", data);
     }
 
     onDispose () {
-        console.log("Dispose AuthRoom");
+        console.log("Dispose Room");
     }
 
 }
