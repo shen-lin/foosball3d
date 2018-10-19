fc169556ffe1ea71f908638ed8415ddd48d89e46
Shen Lin <shen.lin@outlook.com>
2018-10-18 23:02:30 -0700

    join lobby in progress

--- foosball-gaming/src/rooms/standard/StandardRoom.ts
+++ foosball-gaming/src/rooms/standard/StandardRoom.ts
@@ -1,34 +1,75 @@
 import { Room, Client } from "colyseus";
 
 export class StandardRoom extends Room<any> {
-    standard: String
+  standard: String;
 
-    onInit (options) {
-        var oMetadata = {name: options.standard, creator: options.creator, scene: options.scene, mode: options.mode};
-        console.log("StandardRoom created!", options);
-        this.setState({metadata: oMetadata});
-        this.setMetadata(oMetadata);
-        this.standard = options.standard;
-    }
+  onInit(options) {
+    console.log("StandardRoom created", options);
+    var oState = {
+      step: "NEW_ROOM",
+      players: []
+    };
+    this.setState(oState);
 
-    requestJoin (options) {
-        return options.standard === this.standard;
+    var oMetadata = {
+      name: options.standard,
+      scene: options.scene,
+      mode: options.mode,
+      creator: {
+        client_id: options.clientId,
+        client_session_id: options.sessionId,
+        fb_client_id: options.fb_client_id,
+        fb_client_name: options.fb_client_name
       }
+    };
+    this.setMetadata(oMetadata);
 
-    onJoin (client) {
-        console.log("Client Id: ", client.id, "Client session: ",  client.sessionId, "joined successfully");
+    this.standard = options.standard;
+  }
+
+  requestJoin(options) {
+    return options.standard === this.standard;
+  }
+
+  onJoin(client, options) {
+    console.log(
+      "Player Join ",
+      client.id,
+      client.sessionId,
+      options
+    );
+
+    var player = {
+      client_id: client.id,
+      client_session_id: client.sessionId,
+      fb_client_id: options.fb_client_id,
+      fb_client_name: options.fb_client_name
+    };
+
+    this.state.players.push(player);
+
+    if (this.state.step === "NEW_ROOM") {
+      this.state.step = "PLAYER_JOIN";
     }
 
-    onLeave (client) {
-        console.log(client.id, client.sessionId, "left");
-    }
+    // fb_client_id: '10102675149890581',
+    // fb_client_name: 'Shen Lin' },
+  }
 
-    onMessage (client, data) {
-        console.log("Room received message from client session", client.sessionId, ":", data);
-    }
+  onLeave(client) {
+    console.log(client.id, client.sessionId, "left");
+  }
 
-    onDispose () {
-        console.log("Dispose Room");
-    }
+  onMessage(client, data) {
+    console.log(
+      "Room received message from client session",
+      client.sessionId,
+      ":",
+      data
+    );
+  }
 
+  onDispose() {
+    console.log("Dispose Room");
+  }
 }
