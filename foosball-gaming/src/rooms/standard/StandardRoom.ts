04d57e3f7893cf727fb6caacc5b399150c5f3a90
Shen Lin <shen.lin@outlook.com>
2018-10-21 20:15:40 -0700

    waiting lobby in progress

--- foosball-gaming/src/rooms/standard/StandardRoom.ts
+++ foosball-gaming/src/rooms/standard/StandardRoom.ts
@@ -6,15 +6,11 @@ export class StandardRoom extends Room<any> {
 
   onInit(options) {
     console.log("StandardRoom created", options);
-    let state = new GameState();
-    state.step = "NEW_ROOM";
-    state.players = [];
-    this.setState(state);
 
     let creator = new Player();
     creator.client_id = options.clientId;
     creator.client_session_id = options.sessionId;
-    creator.fb_client_id = options.fb_client_id;
+    creator.fb_client_id = options.fb_client_id.replace("FB", "");;
     creator.fb_client_name = options.fb_client_name;
 
     let metadata = new RoomModel();
@@ -22,6 +18,12 @@ export class StandardRoom extends Room<any> {
     metadata.scene = options.scene;
     metadata.mode = options.mode;
     metadata.creator = creator;
+
+    let state = new GameState();
+    state.step = "NEW_ROOM";
+    state.players = [];
+    state.roominfo = metadata;
+    this.setState(state);
 
     this.setMetadata(metadata);
     this.standard = options.standard;
@@ -32,18 +34,17 @@ export class StandardRoom extends Room<any> {
   }
 
   onJoin(client, options) {
-    console.log("Player Join ", client.id, client.sessionId);
-
     let player = new Player();
     player.client_id = client.id;
     player.client_session_id = client.sessionId;
-    player.fb_client_id = options.fb_client_id;
+    player.fb_client_id = options.fb_client_id.replace("FB", "");
     player.fb_client_name = options.fb_client_name;
 
-    this.state.players.push(player);
     if (this.state.step === "NEW_ROOM") {
       this.state.step = "PLAYER_JOIN";
     }
+    console.log("Player Join", player);
+    this.state.players.push(player);
   }
 
   onLeave(client) {
--- foosball-model/model/GameState.ts
+++ foosball-model/model/GameState.ts
@@ -1,6 +1,8 @@
 import Player from "./Player";
+import Room from "./Room";
 
 export default class GameState {
   step: string;
   players: Array<Player>;
+  roominfo: Room;
 }
