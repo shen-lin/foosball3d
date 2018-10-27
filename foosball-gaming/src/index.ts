7b28008dba5683a842c4372b2d8ef4966786d25f
Shen Lin <shen.lin@outlook.com>
2018-10-27 11:41:23 -0700

    correst gaming server commmuncation

--- foosball-gaming/src/index.ts
+++ foosball-gaming/src/index.ts
@@ -6,14 +6,13 @@ import { monitor } from "@colyseus/monitor";
 import * as WebSocket from "ws";
 import { StandardRoom } from "./rooms/standard/StandardRoom";
 
-const port = Number(process.env.PORT || 7000);
+const port = process.env.environment ? 433 : 7000;
+
 const gameServer = new Server({
   engine: WebSocket.Server,
   server: createServer()
 });
 
 gameServer.register("standard", StandardRoom);
-
-
 gameServer.listen(port);
 console.log(`Listening on http://localhost:${port}`);
